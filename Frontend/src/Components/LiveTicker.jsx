048ed6c1b7f78ea3dd3c3faa6875680d8cbd6f3c
Athil c <athilchalil@gmail.com>
2025-10-13 17:58:57 +0530

    update

--- Frontend/src/Components/LiveTicker.jsx
+++ Frontend/src/Components/LiveTicker.jsx
@@ -24,12 +24,12 @@ const LiveTicker = () => {
       <div className="relative w-full h-10 overflow-hidden">
         <motion.div
           className="absolute whitespace-nowrap"
-          initial={{ x: '100%' }} // Start at the right
+          initial={{ x: '50%' }} // Start at the right
           animate={{ x: '-100%' }} // Move to the left
           transition={{
             repeat: Infinity,
             ease: 'linear',
-            duration: 110, // Speed of the scroll
+            duration: 40, // Speed of the scroll
           }}
         >
           {updates.map((text, idx) => (
