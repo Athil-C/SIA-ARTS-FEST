89d8c640051abe4e399e016c8ab6e09bb78a2bc0
Athil c <athilchalil@gmail.com>
2025-10-17 22:10:16 +0530

    update

--- Frontend/src/Components/Gallery.jsx
+++ Frontend/src/Components/Gallery.jsx
@@ -34,6 +34,42 @@ import img28 from "../assets/gallery/img28.jpg";
 import img29 from "../assets/gallery/img29.jpg";
 import img30 from "../assets/gallery/img30.jpg";
 import img31 from "../assets/gallery/img31.jpg";
+import img32 from "../assets/gallery/img32.png";
+import img33 from "../assets/gallery/img33.png";
+import img34 from "../assets/gallery/img34.png";
+import img35 from "../assets/gallery/img35.png";
+import img36 from "../assets/gallery/img36.png";
+import img37 from "../assets/gallery/img37.png";
+import img38 from "../assets/gallery/img38.png";
+import img39 from "../assets/gallery/img39.png";
+import img40 from "../assets/gallery/img40.png";
+import img41 from "../assets/gallery/img41.png";
+import img42 from "../assets/gallery/img42.png";
+import img43 from "../assets/gallery/img43.png";
+import img44 from "../assets/gallery/img44.png";
+import img45 from "../assets/gallery/img45.png";
+import img46 from "../assets/gallery/img46.png";
+import img47 from "../assets/gallery/img47.png";
+import img48 from "../assets/gallery/img48.png";
+import img49 from "../assets/gallery/img49.png";
+import img50 from "../assets/gallery/img50.png";
+import img51 from "../assets/gallery/img51.png";
+import img52 from "../assets/gallery/img52.png";
+import img53 from "../assets/gallery/img53.png";
+import img54 from "../assets/gallery/img54.png";
+import img55 from "../assets/gallery/img55.png";
+import img56 from "../assets/gallery/img56.png";
+import img57 from "../assets/gallery/img57.png";
+import img58 from "../assets/gallery/img58.png";
+import img59 from "../assets/gallery/img59.png";
+import img60 from "../assets/gallery/img60.png";
+import img61 from "../assets/gallery/img61.png";
+import img62 from "../assets/gallery/img62.png";
+import img63 from "../assets/gallery/img63.png";
+import img64 from "../assets/gallery/img64.png";
+import img65 from "../assets/gallery/img65.png";
+import img66 from "../assets/gallery/img66.png";
+
 
 
 
@@ -44,7 +80,7 @@ const Gallery = () => {
 
 
 
-    const images = [  img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31
+    const images = [ , img32, img33, img34, img35, img36, img37, img38, img39, img40, img41, img42, img43, img44, img45, img46, img47, img48, img49, img50, img51, img52, img53, img54, img55, img56, img57, img58, img59, img60, img61, img62, img63, img64, img65, img66,img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31,
 ];
 
 
