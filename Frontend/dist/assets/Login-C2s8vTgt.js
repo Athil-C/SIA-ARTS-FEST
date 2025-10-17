55420f81c9aafe154b9facbbf7e6a09c29370949
Athil c <athilchalil@gmail.com>
2025-10-17 22:42:55 +0530

    update

---
+++ Frontend/dist/assets/Login-C2s8vTgt.js
@@ -0,0 +1,1 @@
+import{j as s}from"./animations-BXiUXQ7_.js";import"./vendor-CCvBl20k.js";import{A as o}from"./index-C4qNhcdM.js";const i=()=>s.jsx("section",{children:s.jsx(o,{})});export{i as default};
--- Frontend/dist/assets/Login-CWjbaVU8.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./animations-BXiUXQ7_.js";import"./vendor-CCvBl20k.js";import{A as o}from"./index-BaNT_tjX.js";const i=()=>s.jsx("section",{children:s.jsx(o,{})});export{i as default};
